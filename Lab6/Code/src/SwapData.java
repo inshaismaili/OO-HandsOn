1649bca98dfacd297f4f4d7cb924606ee53f867a
insha <insha@DESKTOP-T264ER5>
2020-07-16 20:32:04 +0530

    Completed the logic

--- Lab6/Code/src/SwapData.java
+++ Lab6/Code/src/SwapData.java
@@ -1,19 +1,35 @@
 public class SwapData {
-	private int arg1;
-	private int arg2;
+	private String arg1;
+	private String arg2;
 
 	public void swapValues() {
-
+       String temp;
+       temp=arg1;
+       arg1=arg2;
+       arg2=temp;
 	//Logic to swap values
 	
 	}
 
 	public void displayValues(String str) {
-
-	//Display string message with values of arg1 and arg2
+    System.out.println(str);
+    System.out.println("Value1:"+arg1);
+    System.out.println("Value2:"+arg2);
+    //Display string message with values of arg1 and arg2
 
 	}
+  
+	public void  setArg1(String arg1) {
+		this.arg1=arg1;
+	}
+	public void  setArg2(String arg2) {
+		this.arg2=arg2;
+	}
+	public String getArg1() {
+		return arg1;
+	}
+	public String getArg2() {
+		return arg2;
+		// Create setter and getter methods 
 
-// Create setter and getter methods 
-
-}
+}}
--- Lab6/Code/src/TestMain.java
+++ Lab6/Code/src/TestMain.java
@@ -2,12 +2,15 @@ public class TestMain {
 	public static void main(String[] args) {
 		SwapData obj = new SwapData();
 
-		int arg1 = 0;
-		int arg2 = 0;
-
+		
+  System.out.println("Enter value of  arg1:");
+ String arg1=Console.readLine();
+ System.out.println("Enter value of args2:");
+ String arg2=Console.readLine();
 //Read values of arg1 and arg2 from Console
 
-
+       obj.setArg1(arg1);
+       obj.setArg2(arg2);
 // Call set methods of SwapData object
 
 		obj.displayValues("Before Swapping...");
