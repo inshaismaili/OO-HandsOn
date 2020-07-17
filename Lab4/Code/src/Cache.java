6bee64398efd4930b297945a333bc02b147ce717
insha <insha@DESKTOP-T264ER5>
2020-07-17 23:40:08 +0530

    Implemeted the static block in cache class

--- Lab4/Code/src/Cache.java
+++ Lab4/Code/src/Cache.java
@@ -2,7 +2,10 @@ public class Cache {
 	private static int MAX_CAPACITY = 0;
 	
 	// Implement static block
-
+static {
+	System.out.println("Enter initial max_capacity:");
+	MAX_CAPACITY=Console.readInt();
+}
 	static int getMaxCapacity() {
 		System.out.println("Returning MAX_CAPACITY");
 		return MAX_CAPACITY;
