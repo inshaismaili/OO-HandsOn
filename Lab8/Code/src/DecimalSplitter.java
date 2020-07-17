cbbf6b0ca8a03063fcf285636e5610258faee963
insha <insha@DESKTOP-T264ER5>
2020-07-18 01:03:24 +0530

    Complete logics of Lab8

--- Lab8/Code/src/DecimalSplitter.java
+++ Lab8/Code/src/DecimalSplitter.java
@@ -1,13 +1,20 @@
 public class DecimalSplitter {
-	static boolean isOdd(int num) {
+	private static final double fraction = 0;
+
+	 static boolean isOdd(int num) {
+		return (num%2!=0);//Complete the logic
+		
+		}
+
+	 static int getWhole(double  num) {
 //Complete the logic
-	}
-	static int getWhole(double num) {
-//Complete the logic
+		return (int) num;
 	}
 	
-	static double getFraction(double num) {
+static	double getFraction(double num) {
 //Complete the logic
+	double fraction = num - getWhole(num);
+	return fraction;
 	}
 }
 
--- Lab8/Code/src/TestMain.java
+++ Lab8/Code/src/TestMain.java
@@ -1,19 +1,18 @@
+import java.util.Scanner;
+
 public class TestMain {
 	public static void main(String[] args) {
 		if(args.length == 0) {
 			System.out.println("Pass a double val!");
-			System.exit(0);
+		System.exit(0);
 		}
 		
-		double num = 0;
-		int whole = 0;
-		double fraction = 0;
+		double num = (double) Double.parseDouble(args[0]);
+		int whole = DecimalSplitter.getWhole(num); 
+		double fraction = DecimalSplitter.getFraction(num);
 		
-
-// complete the statements
-
-		System.out.println("Whole : " + whole);
-		System.out.printf("Fraction : %.3f\n" , fraction);
+		System.out.println("Whole : " + DecimalSplitter.getWhole(whole));
+		System.out.printf("Fraction : %.3f\n" , DecimalSplitter.getFraction(fraction));
 
 		System.out.println("The number is " + (DecimalSplitter.isOdd(whole) ? "Odd" : "Even"));
 	}
