c734826cdf56d43ed26d7e8dfcc00e62f9922448
insha <insha@DESKTOP-T264ER5>
2020-07-18 03:54:16 +0530

    Lab 2 Completed and findings has been written

--- Lab2/Code/src/AccManager.java
+++ Lab2/Code/src/AccManager.java
@@ -1,6 +1,14 @@
 public class AccManager {
 	public void fillAccountData(Account a1) {
-	
+	System.out.println("Enter accountno:");
+	String Acc_NO=Console.readLine();
+	System.out.println("Enter Name:");
+	String Name=Console.readLine();
+	System.out.println("Enter balance:");
+	double balance=Console.readDouble();
+	a1.setName(Name);
+	a1.setBalance(balance);
+	a1.setAccNo(Acc_NO);
 	//Complete the statements
 
 	}
